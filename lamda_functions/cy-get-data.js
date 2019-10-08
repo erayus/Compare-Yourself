c868405515cd4341b44877d04c2a4010dc5ca2fd
Erayus <raymondhieutran@gmail.com>
2019-10-09 00:21:27 +1100

    [New] Get UserId using the accessToken passed via API Gateway

--- lamda_functions/cy-get-data.js
+++ lamda_functions/cy-get-data.js
@@ -1,9 +1,11 @@
 const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB({region: 'ap-southeast-2', apiVersion: '2012-08-10'});
-
+const cisp = new AWS.CognitoIdentitySereviceProvider({apiVersion: '2016-04-18'});
 
 
 exports.handler = (event, context, callback) => {
+    const accessToken = event.accessToken;
+
     const type = event.type;
     if (type === 'all'){
         const params = {
@@ -22,23 +24,39 @@ exports.handler = (event, context, callback) => {
             }
         });
     } else if (type === 'single'){
-        const params = {
-          Key:{
-              "UserId": {
-                  S: "User_0.5162124313710601"
-              }
-          },
-          TableName: "compare-yourself"
-        };
-        dynamodb.getItem(params, function(error, data){
-            if(error){
-                console.log(error)
-                callback(error)
-            }else{
-                console.log(data);
-                callback(null, [{age: +data.Item.Age.N, height: +data.Item.Height.N, income: +data.Item.Income.N}]);
-            }
-        })
+        const cispParams = {
+            "AccessToken": accessToken
+        }
+
+        cisp.getUser(cispParams, (err, result) => {
+            if (err) {
+                console.log(err);
+                callback(err)
+            } else {
+                console.log(result);
+                const userId = result.UserAttributes[0].Value;
+                const params = {
+                  Key:{
+                      "UserId": {
+                          S: userId
+                      }
+                  },
+                  TableName: "compare-yourself"
+                };
+                dynamodb.getItem(params, function(error, data){
+                    if(error){
+                        console.log(error)
+                        callback(error)
+                    }else{
+                        console.log(data);
+                        callback(null, [{age: +data.Item.Age.N, height: +data.Item.Height.N, income: +data.Item.Income.N}]);
+                    }
+                })
+                    }
+
+                })
+
+
     } else {
         callback(null, 'Hello From Lambda');
     }
